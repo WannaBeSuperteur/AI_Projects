9d55ffee9863434af7b483636ca34d2dcbd78370
wannabesuperteur <iwant_optimi@naver.com>
2025-03-19 23:19:08 +0900

    ADD: SFT dataset convert code

--- 2025_03_12_DeepSeek_LLM/create_dataset/convert_to_llm_dataset.py
+++ 2025_03_12_DeepSeek_LLM/create_dataset/convert_to_llm_dataset.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import os
 
 # *.csv 데이터셋을 LLM 이 직접 학습 가능한 학습 데이터셋으로 변환
-# Create Date : 2025.03.17
+# Create Date : 2025.03.19
 # Last Update Date : -
 
 # Arguments:
@@ -12,13 +13,25 @@ import pandas as pd
 # - LLM 이 직접 학습 가능한 데이터셋 파일을 저장
 
 def convert_to_llm_data(csv_path, train_option):
-    raise NotImplementedError
+    assert train_option in ['sft', 'orpo']
+
+    if train_option == 'sft':
+        df = pd.read_csv(csv_path)
+        df = df[['input_data', 'output_data']]
+        df = df.sample(frac=1)  # shuffle rows
+
+        save_path = csv_path.replace('_dataset.csv', '_dataset_llm.csv')
+        df.to_csv(save_path, index=False)
+
+    else:  # ORPO
+        raise NotImplementedError
 
 
 if __name__ == '__main__':
+    abs_path = os.path.abspath(os.path.dirname(__file__))
 
     # Supervised Fine-Tuning 데이터셋 생성
-    convert_to_llm_data('sft_dataset.py', 'sft')
+    convert_to_llm_data(f'{abs_path}/sft_dataset.csv', 'sft')
 
     # ORPO 데이터셋 생성
-    convert_to_llm_data('orpo_dataset.csv', 'orpo')
+    convert_to_llm_data(f'{abs_path}/orpo_dataset.csv', 'orpo')
