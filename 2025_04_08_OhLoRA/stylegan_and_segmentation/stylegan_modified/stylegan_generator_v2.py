81804445eaa1960eff4b43db202c64124824577f
wannabesuperteur <iwant_optimi@naver.com>
2025-04-13 21:29:38 +0900

    ADD: StyleGAN Fine-Tuning CNN training code

--- 2025_04_08_OhLoRA/stylegan_and_segmentation/stylegan_modified/stylegan_generator_v2.py
+++ 2025_04_08_OhLoRA/stylegan_and_segmentation/stylegan_modified/stylegan_generator_v2.py
@@ -1,13 +1,23 @@
 import torch
 import torch.nn as nn
 import os
+import pandas as pd
+from torch.utils.data import random_split, DataLoader
 
+from stylegan_modified.fine_tuning import concatenate_property_scores
+from torchinfo import summary
 
 IMG_HEIGHT = 256
 IMG_WIDTH = 256
 
+TRAIN_BATCH_SIZE = 16
+VALID_BATCH_SIZE = 4
+EARLY_STOPPING_ROUNDS = 10
+
 PROJECT_DIR_PATH = os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))))
 INFERENCE_RESULT_DIR = f'{PROJECT_DIR_PATH}/stylegan_and_segmentation/cnn/inference_result'
+
+cnn_loss_func = nn.MSELoss(reduction='mean')
 
 
 # Eyes Score part of CNN
@@ -525,6 +535,9 @@ def define_cnn_model(device):
     cnn_model.to(device)
     cnn_model.device = device
 
+    # print summary of CNN model before training
+    summary(cnn_model, input_size=(TRAIN_BATCH_SIZE, 3, IMG_HEIGHT, IMG_WIDTH))
+
     return cnn_model
 
 
@@ -537,12 +550,78 @@ def define_cnn_model(device):
 # - fine_tuning_dataloader (DataLoader) : StyleGAN Fine-Tuning 용 데이터셋의 Data Loader
 
 # Returns:
-# - cnn_model (nn.Module) : 학습된 CNN 모델
+# - best_epoch_model (nn.Module) : 학습된 CNN 모델 (valid loss 가 가장 작은 best epoch)
 
 def train_cnn_model(device, fine_tuning_dataloader):
     cnn_model = define_cnn_model(device)
 
-    raise NotImplementedError
+    # split dataset
+    dataset_size = len(fine_tuning_dataloader.dataset)
+    train_size = int(dataset_size * 0.9)
+    valid_size = dataset_size - train_size
+
+    train_dataset, valid_dataset = random_split(fine_tuning_dataloader.dataset, [train_size, valid_size])
+    train_loader = DataLoader(train_dataset, batch_size=TRAIN_BATCH_SIZE, shuffle=True)
+    valid_loader = DataLoader(valid_dataset, batch_size=VALID_BATCH_SIZE, shuffle=False)
+
+    # prepare training
+    current_epoch = 0
+    min_val_loss_epoch = -1  # Loss-based Early Stopping
+    min_val_loss = None
+    best_epoch_model = None
+
+    performance_dict = {'epoch': [], 'valid_loss': [],
+                        'eyes_score_loss': [],
+                        'mouth_score_loss': [],
+                        'pose_score_loss': [],
+                        'hair_color_score_loss': [],
+                        'hair_length_score_loss': [],
+                        'back_mean_score_loss': [],
+                        'back_std_score_loss': []}
+
+    # run training until early stopping
+    while True:
+
+        # run train and validation
+        train_loss = train_cnn_train_step(cnn_model=cnn_model,
+                                          cnn_train_dataloader=train_loader)
+
+        valid_log = train_cnn_valid_step(cnn_model=cnn_model,
+                                         cnn_valid_dataloader=valid_loader,
+                                         current_epoch=current_epoch)
+
+        val_loss = valid_log['valid_loss']
+        print(f'epoch : {current_epoch}, train_loss : {train_loss:.4f}, valid_loss : {val_loss:.4f}')
+
+        # update log
+        for k, v in valid_log.items():
+            if k == 'epoch':
+                performance_dict[k].append(v)
+            else:
+                performance_dict[k].append(round(v, 4))
+
+        # save train log
+        train_log_path = f'{PROJECT_DIR_PATH}/stylegan_and_segmentation/stylegan_modified/train_log_v2_cnn.csv'
+        performance_df = pd.DataFrame(performance_dict)
+        performance_df.to_csv(train_log_path, index=False)
+
+        # update scheduler
+        cnn_model.scheduler.step()
+
+        # update best epoch
+        if min_val_loss is None or val_loss < min_val_loss:
+            min_val_loss = val_loss
+            min_val_loss_epoch = current_epoch
+
+            best_epoch_model = PropertyScoreCNN().to(cnn_model.device)
+            best_epoch_model.load_state_dict(cnn_model.state_dict())
+
+        if current_epoch - min_val_loss_epoch >= EARLY_STOPPING_ROUNDS:
+            break
+
+        current_epoch += 1
+
+    return best_epoch_model
 
 
 # CNN 모델의 Train Step
@@ -550,11 +629,36 @@ def train_cnn_model(device, fine_tuning_dataloader):
 # Last Update Date : -
 
 # Arguments:
-# - cnn_model              (nn.Module)  : 학습 중인 CNN 모델
-# - fine_tuning_dataloader (DataLoader) : StyleGAN Fine-Tuning 용 데이터셋의 Data Loader
+# - cnn_model            (nn.Module)  : 학습 중인 CNN 모델
+# - cnn_train_dataloader (DataLoader) : StyleGAN Fine-Tuning 용 데이터셋의 CNN Train 용 Data Loader
 
-def train_cnn_train_step(cnn_model, fine_tuning_dataloader):
-    raise NotImplementedError
+# Returns:
+# - train_loss (float) : 모델의 Training Loss
+
+def train_cnn_train_step(cnn_model, cnn_train_dataloader):
+    cnn_model.train()
+    total = 0
+    train_loss_sum = 0.0
+
+    for idx, raw_data in enumerate(cnn_train_dataloader):
+        images = raw_data['image']
+        labels = concatenate_property_scores(raw_data)
+        images, labels = images.to(cnn_model.device), labels.to(cnn_model.device).to(torch.float32)
+
+        # train 실시
+        cnn_model.optimizer.zero_grad()
+        outputs = cnn_model(images).to(torch.float32)
+
+        loss = cnn_loss_func(outputs, labels)
+        loss.backward()
+        cnn_model.optimizer.step()
+
+        train_loss_current_batch = float(loss.detach().cpu().numpy())
+        train_loss_sum += train_loss_current_batch * labels.size(0)
+        total += labels.size(0)
+
+    train_loss = train_loss_sum / total
+    return train_loss
 
 
 # CNN 모델의 Valid Step
@@ -562,8 +666,9 @@ def train_cnn_train_step(cnn_model, fine_tuning_dataloader):
 # Last Update Date : -
 
 # Arguments:
-# - cnn_model              (nn.Module)  : 학습 중인 CNN 모델
-# - fine_tuning_dataloader (DataLoader) : StyleGAN Fine-Tuning 용 데이터셋의 Data Loader
+# - cnn_model            (nn.Module)  : 학습 중인 CNN 모델
+# - cnn_valid_dataloader (DataLoader) : StyleGAN Fine-Tuning 용 데이터셋의 CNN Validation 용 Data Loader
+# - current_epoch        (int)        : 현재 epoch 번호 (로깅 목적)
 
 # Returns:
 # - valid_log (dict) : CNN 모델의 Validation Log
@@ -571,8 +676,62 @@ def train_cnn_train_step(cnn_model, fine_tuning_dataloader):
 #                       'pose_score_loss': float, 'hair_color_score_loss': float, 'hair_length_score_loss': float,
 #                       'back_mean_score_loss': float, 'back_std_score_loss': float}
 
-def train_cnn_valid_step(cnn_model, fine_tuning_dataloader):
-    raise NotImplementedError
+def train_cnn_valid_step(cnn_model, cnn_valid_dataloader, current_epoch):
+    cnn_model.eval()
+    correct, total = 0, 0
+    val_loss_sum = 0
+
+    valid_log = {'epoch': current_epoch,
+                 'eyes_score_loss': 0.0,
+                 'mouth_score_loss': 0.0,
+                 'pose_score_loss': 0.0,
+                 'hair_color_score_loss': 0.0,
+                 'hair_length_score_loss': 0.0,
+                 'back_mean_score_loss': 0.0,
+                 'back_std_score_loss': 0.0}
+
+    with torch.no_grad():
+        for idx, raw_data in enumerate(cnn_valid_dataloader):
+            images = raw_data['image']
+            labels = concatenate_property_scores(raw_data)
+            images, labels = images.to(cnn_model.device), labels.to(cnn_model.device).to(torch.float32)
+
+            outputs = cnn_model(images).to(torch.float32)
+
+            val_loss_current_batch = float(cnn_loss_func(outputs, labels).detach().cpu().numpy())
+            val_loss_sum += val_loss_current_batch * labels.size(0)
+            total += labels.size(0)
+
+            # compute detailed losses
+            eyes_score_loss = float(cnn_loss_func(outputs[:, :1], labels[:, :1]).detach().cpu().numpy())
+            mouth_score_loss = float(cnn_loss_func(outputs[:, 1:2], labels[:, 1:2]).detach().cpu().numpy())
+            pose_score_loss = float(cnn_loss_func(outputs[:, 2:3], labels[:, 2:3]).detach().cpu().numpy())
+            hair_color_score_loss = float(cnn_loss_func(outputs[:, 3:4], labels[:, 3:4]).detach().cpu().numpy())
+            hair_length_score_loss = float(cnn_loss_func(outputs[:, 4:5], labels[:, 4:5]).detach().cpu().numpy())
+            back_mean_score_loss = float(cnn_loss_func(outputs[:, 5:6], labels[:, 5:6]).detach().cpu().numpy())
+            back_std_score_loss = float(cnn_loss_func(outputs[:, 6:], labels[:, 6:]).detach().cpu().numpy())
+
+            valid_log['eyes_score_loss'] += eyes_score_loss * labels.size(0)
+            valid_log['mouth_score_loss'] += mouth_score_loss * labels.size(0)
+            valid_log['pose_score_loss'] += pose_score_loss * labels.size(0)
+            valid_log['hair_color_score_loss'] += hair_color_score_loss * labels.size(0)
+            valid_log['hair_length_score_loss'] += hair_length_score_loss * labels.size(0)
+            valid_log['back_mean_score_loss'] += back_mean_score_loss * labels.size(0)
+            valid_log['back_std_score_loss'] += back_std_score_loss * labels.size(0)
+
+        # Final Loss 계산
+        val_loss = val_loss_sum / total
+        valid_log['valid_loss'] = val_loss
+
+        valid_log['eyes_score_loss'] /= total
+        valid_log['mouth_score_loss'] /= total
+        valid_log['pose_score_loss'] /= total
+        valid_log['hair_color_score_loss'] /= total
+        valid_log['hair_length_score_loss'] /= total
+        valid_log['back_mean_score_loss'] /= total
+        valid_log['back_std_score_loss'] /= total
+
+    return valid_log
 
 
 # 학습된 CNN 모델 불러오기
