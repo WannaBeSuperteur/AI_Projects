9c3f699c6681e40da2846e45c5cfe0087873a608
wannabesuperteur <iwant_optimi@naver.com>
2025-04-14 12:33:33 +0900

    FIX: pose score CNN

--- 2025_04_08_OhLoRA/stylegan_and_segmentation/stylegan_modified/stylegan_generator_v2.py
+++ 2025_04_08_OhLoRA/stylegan_and_segmentation/stylegan_modified/stylegan_generator_v2.py
@@ -186,10 +186,15 @@ class PoseScoreCNN(nn.Module):
             nn.LeakyReLU(),
             nn.Dropout2d(0.05)
         )
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(128, 128, kernel_size=3),
+            nn.LeakyReLU(),
+            nn.Dropout2d(0.05)
+        )
 
         # Fully Connected Layers
         self.fc1 = nn.Sequential(
-            nn.Linear(128 * 8 * 8, 256),
+            nn.Linear(128 * 14 * 6, 256),
             nn.Tanh(),
             nn.Dropout(0.45)
         )
@@ -197,16 +202,17 @@ class PoseScoreCNN(nn.Module):
 
     def forward(self, x):
         # Conv
-        x = self.conv1(x)  # 46 x 46
-        x = self.conv2(x)  # 44 x 44
-        x = self.pool1(x)  # 22 x 22
+        x = self.conv1(x)  # 78 x 46
+        x = self.conv2(x)  # 76 x 44
+        x = self.pool1(x)  # 38 x 22
 
-        x = self.conv3(x)  # 20 x 20
-        x = self.pool2(x)  # 10 x 10
+        x = self.conv3(x)  # 36 x 20
+        x = self.pool2(x)  # 18 x 10
 
-        x = self.conv4(x)  # 8 x 8
+        x = self.conv4(x)  # 16 x 8
+        x = self.conv5(x)  # 14 x 6
 
-        x = x.view(-1, 128 * 8 * 8)
+        x = x.view(-1, 128 * 14 * 6)
 
         # Fully Connected
         x = self.fc1(x)
@@ -469,7 +475,7 @@ class PropertyScoreCNN(nn.Module):
     def forward(self, x, tensor_visualize_test=False):
         x_eyes = x[:, :, 3 * IMG_HEIGHT // 8 : 9 * IMG_HEIGHT // 16, IMG_WIDTH // 4 : 3 * IMG_WIDTH // 4]
         x_mouth = x[:, :, 5 * IMG_HEIGHT // 8 : 13 * IMG_HEIGHT // 16, 3 * IMG_WIDTH // 8 : 5 * IMG_WIDTH // 8]
-        x_nose = x[:, :, 15 * IMG_HEIGHT // 32 : 21 * IMG_HEIGHT // 32, 13 * IMG_WIDTH // 32 : 19 * IMG_WIDTH // 32]
+        x_pose = x[:, :, 7 * IMG_HEIGHT // 16 : 5 * IMG_HEIGHT // 8, 11 * IMG_WIDTH // 32 : 21 * IMG_WIDTH // 32]
         x_entire = x
         x_bottom_half = x[:, :, IMG_HEIGHT // 2 :, :]
         x_upper_half = x[:, :, : IMG_HEIGHT // 2, :]
@@ -481,7 +487,7 @@ class PropertyScoreCNN(nn.Module):
             for i in range(current_batch_size):
                 save_tensor_png(x_eyes[i], image_save_path=f'{CNN_TENSOR_TEST_DIR}/eyes_{i:03d}.png')
                 save_tensor_png(x_mouth[i], image_save_path=f'{CNN_TENSOR_TEST_DIR}/mouth_{i:03d}.png')
-                save_tensor_png(x_nose[i], image_save_path=f'{CNN_TENSOR_TEST_DIR}/nose_{i:03d}.png')
+                save_tensor_png(x_pose[i], image_save_path=f'{CNN_TENSOR_TEST_DIR}/pose_{i:03d}.png')
                 save_tensor_png(x_entire[i], image_save_path=f'{CNN_TENSOR_TEST_DIR}/entire_{i:03d}.png')
                 save_tensor_png(x_bottom_half[i], image_save_path=f'{CNN_TENSOR_TEST_DIR}/b_half_{i:03d}.png')
                 save_tensor_png(x_upper_half[i], image_save_path=f'{CNN_TENSOR_TEST_DIR}/u_half_{i:03d}.png')
@@ -489,13 +495,13 @@ class PropertyScoreCNN(nn.Module):
         # Compute Each Score
         x_eyes = self.eyes_score_cnn(x_eyes)
         x_mouth = self.mouth_score_cnn(x_mouth)
-        x_nose = self.pose_score_cnn(x_nose)
+        x_pose = self.pose_score_cnn(x_pose)
         x_entire = self.hair_color_score_cnn(x_entire)
         x_bottom_half = self.hair_length_score_cnn(x_bottom_half)
         x_upper_half = self.background_score_cnn(x_upper_half)
 
         # Final Concatenate
-        x_final = torch.concat([x_eyes, x_mouth, x_nose, x_entire, x_bottom_half, x_upper_half], dim=1)
+        x_final = torch.concat([x_eyes, x_mouth, x_pose, x_entire, x_bottom_half, x_upper_half], dim=1)
         return x_final
 
 
@@ -637,9 +643,9 @@ def train_cnn_train_step(cnn_model, cnn_train_dataloader):
         loss.backward()
         cnn_model.optimizer.step()
 
-        if idx % 5 == 0:
-            print(idx, 'train outputs:\n', outputs[:4])
-            print(idx, 'train labels:\n', labels[:4])
+#        if idx % 5 == 0:
+#            print(idx, 'train outputs:\n', outputs[:4])
+#            print(idx, 'train labels:\n', labels[:4])
 
 
 # CNN 모델의 Valid Step
@@ -694,9 +700,9 @@ def train_cnn_valid_step(cnn_model, cnn_valid_dataloader, current_epoch):
             # compute detailed losses and abs. diff info
             compute_detailed_valid_results(outputs, labels, valid_log)
 
-            if idx % 5 == 0:
-                print(idx, 'valid outputs:\n', outputs)
-                print(idx, 'valid labels:\n', labels)
+#            if idx % 5 == 0:
+#                print(idx, 'valid outputs:\n', outputs)
+#                print(idx, 'valid labels:\n', labels)
 
         # Final Loss 계산
         val_loss = val_loss_sum / total
