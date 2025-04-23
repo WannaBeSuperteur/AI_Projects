f2e7c03b450a4eabed2fcd8dd8e82c52afdd457d
wannabesuperteur <iwant_optimi@naver.com>
2025-04-23 12:58:02 +0900

    ADD: S-BERT training code

--- 2025_04_08_OhLoRA/llm/memory_mechanism/inference_sbert.py
+++ 2025_04_08_OhLoRA/llm/memory_mechanism/inference_sbert.py
@@ -1,3 +1,8 @@
+import numpy as np
+import pandas as pd
+
+import os
+PROJECT_DIR_PATH = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 
 
 # Memory Mechanism 을 위한 학습된 S-BERT (Sentence BERT) 모델을 이용하여 "데이터셋 전체에 대한" inference 실시
@@ -10,10 +15,40 @@
 
 # Returns:
 # - 반환값 없음
-# - 테스트 결과 (성능지표 값) 출력됨
+# - 테스트 결과 (성능지표 값) 출력 및 해당 결과를 llm/memory_mechanism/test_result.csv 로 저장
 
 def run_inference(sbert_model, test_dataset_df):
-    raise NotImplementedError
+    n = len(test_dataset_df)
+
+    memory_info_list = test_dataset_df['memory_0'].tolist()
+    user_prompt_list = test_dataset_df['user_prompt_1'].tolist()
+
+    ground_truth_scores = test_dataset_df['similarity_score'].tolist()
+    predicted_scores = []
+
+    # run prediction using trained S-BERT
+    for memory_info, user_prompt in zip(memory_info_list, user_prompt_list):
+        predicted_score = run_inference_each_example(sbert_model, memory_info, user_prompt)
+        predicted_scores.append(predicted_score)
+
+    # compute errors
+    absolute_errors = [abs(predicted_scores[i] - ground_truth_scores[i]) for i in range(n)]
+
+    mse_error = sum([(predicted_scores[i] - ground_truth_scores[i]) ** 2 for i in range(n)]) / n
+    mae_error = sum(absolute_errors) / n
+    corr_coef = np.corrcoef(predicted_scores, ground_truth_scores)[0][1]
+
+    print(f'MSE error : {mse_error:.4f}')
+    print(f'MAE error : {mae_error:.4f}')
+    print(f'Corr-coef : {corr_coef:.4f}')
+
+    # save test result
+    test_result_dict = {'memory': memory_info_list, 'user_prompt': user_prompt_list,
+                        'predicted_score': predicted_scores, 'ground_truth_score': ground_truth_scores,
+                        'absolute_error': absolute_errors}
+
+    test_result_df = pd.DataFrame(test_result_dict)
+    test_result_df.to_csv(f'{PROJECT_DIR_PATH}/llm/memory_mechanism/test_result.csv', index=False)
 
 
 # Memory Mechanism 을 위한 학습된 S-BERT (Sentence BERT) 모델을 이용하여 "각 example 에 대한" inference 실시
