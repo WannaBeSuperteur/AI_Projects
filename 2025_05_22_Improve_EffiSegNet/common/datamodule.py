c3fd8c03a50c72f92b89433802fab96cf045ec91
wannabesuperteur <iwant_optimi@naver.com>
2025-05-24 14:19:58 +0900

    ADD: test result (Dice 0.9386, IoU 0.8904)

--- 2025_05_22_Improve_EffiSegNet/common/datamodule.py
+++ 2025_05_22_Improve_EffiSegNet/common/datamodule.py
@@ -204,16 +204,15 @@ class KvasirSEGDatasetForImprovedModel(L.LightningDataModule):
                 A.HorizontalFlip(p=0.5),
                 A.VerticalFlip(p=0.5),
                 A.ColorJitter(
-                    p=0.8, brightness=(0.6, 1.6), contrast=0.2, saturation=0.1, hue=0.01
+                    p=0.5, brightness=(0.6, 1.6), contrast=0.2, saturation=0.1, hue=0.01
                 ),
                 A.Affine(
-                    p=0.8,
+                    p=0.5,
                     scale=(0.5, 1.5),
                     translate_percent=0.125,
                     rotate=90,
                     interpolation=cv2.INTER_LANCZOS4,
                 ),
-                A.ElasticTransform(p=0.5, interpolation=cv2.INTER_LANCZOS4),
                 A.Normalize(
                     mean=(0.485, 0.456, 0.406),
                     std=(0.229, 0.224, 0.225),
