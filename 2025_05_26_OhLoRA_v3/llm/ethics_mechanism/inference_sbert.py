3e55a2e98d115e3f2993af389ce2e4c3d3c02151
wannabesuperteur <iwant_optimi@naver.com>
2025-06-02 22:19:32 +0900

    ADD: confusion matrix

--- 2025_05_26_OhLoRA_v3/llm/ethics_mechanism/inference_sbert.py
+++ 2025_05_26_OhLoRA_v3/llm/ethics_mechanism/inference_sbert.py
@@ -4,10 +4,14 @@ import pandas as pd
 import os
 PROJECT_DIR_PATH = os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))))
 
+categories = ['사랑 고백/만남', '일반', '정치', '패드립']
+n_categories = len(categories)
+
 
 # Ethics Mechanism 을 위한 학습된 S-BERT (Sentence BERT) 모델을 이용하여 "데이터셋 전체에 대한" inference 실시
 # Create Date : 2025.06.02
-# Last Update Date : -
+# Last Update Date : 2025.06.02
+# - Confusion Matrix 추가
 
 # Arguments:
 # - sbert_model     (S-BERT Model)     : 학습된 Sentence BERT 모델
@@ -16,6 +20,7 @@ PROJECT_DIR_PATH = os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspa
 # Returns:
 # - 반환값 없음
 # - 테스트 결과 (성능지표 값) 출력 및 해당 결과를 llm/ethics_mechanism/test_result.csv 로 저장
+# - 테스트 결과 (성능지표 값) 의 Confusion Matrix 출력 및 해당 결과를 llm/ethics_mechanism/test_confusion_matrix.csv 로 저장
 
 def run_inference(sbert_model, test_dataset_df):
     n = len(test_dataset_df)
@@ -50,6 +55,84 @@ def run_inference(sbert_model, test_dataset_df):
 
     test_result_df = pd.DataFrame(test_result_dict)
     test_result_df.to_csv(f'{PROJECT_DIR_PATH}/llm/ethics_mechanism/test_result.csv', index=False)
+
+    # save confusion matrix
+    confusion_matrix = compute_confusion_matrix(test_result_dict)
+    print(f'\nConfusion Matrix:\n{confusion_matrix}')
+    confusion_matrix.to_csv(f'{PROJECT_DIR_PATH}/llm/ethics_mechanism/test_confusion_matrix.csv', index=False)
+
+
+# Confusion Matrix 도출
+# Create Date : 2025.06.02
+# Last Update Date : -
+
+# Arguments:
+# test_result_dict (dict) : 테스트 결과에 대한 Dictionary
+
+# Returns:
+# confusion_matrix (Pandas DataFrame) : 테스트 결과에 대한 Confusion Matrix
+
+def compute_confusion_matrix(test_result_dict):
+    global categories, n_categories
+
+    # confusion matrix format
+    confusion_matrix_dict = {'pred | true': categories + ['total', 'recall']}
+    for category in categories:
+        confusion_matrix_dict[category] = [0 for _ in range(n_categories)] + [0, 0]
+    confusion_matrix_dict['total'] = [0 for _ in range(n_categories)] + [0, '']
+    confusion_matrix_dict['precision'] = [0 for _ in range(n_categories)] + ['', 0]
+
+    # compute confusion matrix
+    n_test_results = len(test_result_dict['user_prompt'])
+    n_test_cases = n_test_results // n_categories
+
+    for tc_idx in range(n_test_cases):
+        max_pred = test_result_dict['predicted_score'][tc_idx * n_categories]
+        max_pred_idx = 0
+
+        max_gt = test_result_dict['ground_truth_score'][tc_idx * n_categories]
+        max_gt_category = categories[0]
+
+        for ctg_idx in range(n_categories):
+            if test_result_dict['predicted_score'][tc_idx * n_categories + ctg_idx] > max_pred:
+                max_pred = test_result_dict['predicted_score'][tc_idx * n_categories + ctg_idx]
+                max_pred_idx = ctg_idx
+
+            if test_result_dict['ground_truth_score'][tc_idx * n_categories + ctg_idx] > max_gt:
+                max_gt = test_result_dict['ground_truth_score'][tc_idx * n_categories + ctg_idx]
+                max_gt_category = categories[ctg_idx]
+
+        num_after_add = int(confusion_matrix_dict[max_gt_category][max_pred_idx]) + 1
+        confusion_matrix_dict[max_gt_category][max_pred_idx] = str(num_after_add)
+
+    # finalize computing confusion matrix
+    # 1. recall
+    for category_idx, category in enumerate(categories):
+        confusion_matrix_dict[category][n_categories] = sum(map(int, confusion_matrix_dict[category][:n_categories]))
+        correct_cnt = int(confusion_matrix_dict[category][category_idx])
+        total_cnt = int(confusion_matrix_dict[category][n_categories])
+        confusion_matrix_dict[category][n_categories + 1] = round(correct_cnt / total_cnt, 4)
+
+    # 2. total for precision & total test case count
+    for category_idx in range(n_categories):
+        for ctg in categories:
+            confusion_matrix_dict['total'][category_idx] += int(confusion_matrix_dict[ctg][category_idx])
+            confusion_matrix_dict['total'][n_categories] = n_test_cases
+
+    # 3. precision
+    for category_idx, category in enumerate(categories):
+        correct_cnt = int(confusion_matrix_dict[category][category_idx])
+        total_cnt = int(confusion_matrix_dict['total'][category_idx])
+        confusion_matrix_dict['precision'][category_idx] = round(correct_cnt / total_cnt, 4)
+
+    # 4. accuracy
+    sum_correct = 0
+    for category_idx, category in enumerate(categories):
+        sum_correct += int(confusion_matrix_dict[category][category_idx])
+    confusion_matrix_dict['precision'][n_categories + 1] = f'accuracy : {round(sum_correct / n_test_cases, 4)}'
+
+    confusion_matrix = pd.DataFrame(confusion_matrix_dict)
+    return confusion_matrix
 
 
 # Ethics Mechanism 을 위한 학습된 S-BERT (Sentence BERT) 모델을 이용하여 "각 example 에 대한" inference 실시
