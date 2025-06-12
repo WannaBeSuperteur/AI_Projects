cb99f56ae3e32591f1d53be89a73133c9209a172
wannabesuperteur <iwant_optimi@naver.com>
2025-06-12 10:00:20 +0900

    ADD: test result (Gradient with mapping_split1 layer, 1-2K)

--- 2025_06_07_OhLoRA_v3_1/stylegan/run_stylegan_vectorfind_v9_test.py
+++ 2025_06_07_OhLoRA_v3_1/stylegan/run_stylegan_vectorfind_v9_test.py
@@ -219,7 +219,7 @@ def run_stylegan_vectorfind_v9_automated_test_gradient(n, layer_name):
 if __name__ == '__main__':
     ns = [1000, 2000]
     ratios = [0.0, 0.0, 0.0, 0.0, 0.0]
-    layer_name = 'w'
+    layer_name = 'mapping_split1'
 
     for n, ratio in zip(ns, ratios):
         run_stylegan_vectorfind_v9_automated_test_gradient(n, layer_name)
