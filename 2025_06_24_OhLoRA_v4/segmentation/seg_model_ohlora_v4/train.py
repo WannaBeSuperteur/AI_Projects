51575f4499e6d05a0f5a081191a8f01caa6761e1
wannabesuperteur <iwant_optimi@naver.com>
2025-06-25 12:36:21 +0900

    ADD: skeleton code of Oh-LoRA v4 segmentation model (2)

--- 2025_06_24_OhLoRA_v4/segmentation/seg_model_ohlora_v4/train.py
+++ 2025_06_24_OhLoRA_v4/segmentation/seg_model_ohlora_v4/train.py
@@ -1,5 +1,6 @@
 
 import cv2
+import torch
 from torch.utils.data import DataLoader, Dataset
 
 import os
@@ -20,6 +21,20 @@ class OhLoRAV4SegmentationModelDataset(Dataset):
         mask = cv2.imread(self.mask_paths[idx])
 
         return image, mask
+
+
+# Oh-LoRA v4 용 경량화된 Segmentation Model 정의
+# Create Date : 2025.06.25
+# Last Update Date : -
+
+# Arguments:
+# - 없음
+
+# Returns:
+# - model (nn.Module) : 경량화 모델
+
+def define_segmentation_model():
+    raise NotImplementedError
 
 
 # Oh-LoRA v4 용 경량화된 Segmentation Model 의 데이터셋 생성
@@ -46,6 +61,9 @@ def generate_dataset(dataset_path):
 # - model            (nn.Module)  : 경량화 모델
 # - train_dataloader (DataLoader) : 경량화 모델의 Train Data Loader
 # - valid_dataloader (DataLoader) : 경량화 모델의 Valid Data Loader
+
+# Returns:
+# - best_epoch_model (nn.Module) : best epoch (Loss 최소) 에서의 Segmentation Model
 
 def train_model(model, train_dataloader, valid_dataloader):
     raise NotImplementedError
@@ -107,3 +125,10 @@ def test_model(model, test_dataloader):
 if __name__ == '__main__':
     dataset_path = f'{PROJECT_DIR_PATH}/stylegan/generated_face_images_filtered'
     train_dataloader, valid_dataloader, test_dataloader = generate_dataset(dataset_path)
+    model = define_segmentation_model()
+
+    best_epoch_model = train_model(model, train_dataloader, valid_dataloader)
+    test_result_dict = test_model(model, test_dataloader)
+
+    model_save_path = f'{PROJECT_DIR_PATH}/segmenation/models/segmentation_model_ohlora_v4'
+    torch.save(best_epoch_model.state_dict(), model_save_path)
