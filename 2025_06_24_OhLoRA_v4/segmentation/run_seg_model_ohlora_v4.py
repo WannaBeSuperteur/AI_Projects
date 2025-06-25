fd30a9a09139a8747a7d3c39f6970a73d3a6891f
wannabesuperteur <iwant_optimi@naver.com>
2025-06-25 11:24:29 +0900

    ADD: inference time record

--- 2025_06_24_OhLoRA_v4/segmentation/run_seg_model_ohlora_v4.py
+++ 2025_06_24_OhLoRA_v4/segmentation/run_seg_model_ohlora_v4.py
@@ -1,4 +1,8 @@
 
+import time
+import numpy as np
+import torch
+
 from seg_model_ohlora_v4.model import SegModelForOhLoRAV4
 from common import save_model_structure_pdf
 from torchinfo import summary
@@ -28,7 +32,32 @@ def save_model_structure_pdf_files(seg_model):
                                  depth=depth)
 
 
+# 경량화된 Segmentation Model 실행 시간 측정
+# Create Date : 2025.06.25
+# Last Update Date : -
+
+# Arguments:
+# - seg_model (nn.Module) : Oh-LoRA v4 용 경량화된 Segmentation Model
+
+def measure_inference_time(seg_model):
+    inference_time_record = []
+    random_tensor = torch.randn(1, 3, SEG_IMAGE_SIZE, SEG_IMAGE_SIZE)
+
+    for _ in range(100):
+        start_at = time.time()
+        seg_model(random_tensor.cuda())
+        inference_time = time.time() - start_at
+        inference_time_record.append(inference_time)
+
+    print(f'max        inference time : {max(inference_time_record)}')
+    print(f'min        inference time : {min(inference_time_record)}')
+    print(f'mean    of inference time : {np.mean(inference_time_record)}')
+    print(f'std-dev of inference time : {np.std(inference_time_record)}')
+    print(f'median  of inference time : {np.median(inference_time_record)}')
+
+
 if __name__ == '__main__':
     seg_model = SegModelForOhLoRAV4()
     save_model_structure_pdf_files(seg_model)
+    measure_inference_time(seg_model)
 
