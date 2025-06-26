e1dd525aefcbbe0f50c0cbc528f26e523d0805de
wannabesuperteur <iwant_optimi@naver.com>
2025-06-26 12:45:43 +0900

    ADD: ombre image generation (2)

--- 2025_06_24_OhLoRA_v4/ombre/generate_ombre_images.py
+++ 2025_06_24_OhLoRA_v4/ombre/generate_ombre_images.py
@@ -1,10 +1,10 @@
 import torch
-from torchvision.io import read_image
+import torchvision.transforms as transforms
 
 import os
 import sys
 import numpy as np
-import pandas as pd
+import cv2
 
 PROJECT_DIR_PATH = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 sys.path.append(PROJECT_DIR_PATH)
@@ -40,6 +40,12 @@ PROPERTY_NAMES = ['eyes', 'mouth', 'pose']
 
 kwargs_val = dict(trunc_psi=1.0, trunc_layers=0, randomize_noise=False)
 
+seg_model_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=0.5, std=0.5)
+])
+
 
 
 # 옴브레 염색 적용된 이미지 생성
@@ -48,6 +54,7 @@ kwargs_val = dict(trunc_psi=1.0, trunc_layers=0, randomize_noise=False)
 
 # Arguments:
 # - vectorfind_generator (nn.Module)   : StyleGAN-VectorFind-v7 or v8 의 Generator
+# - hair_seg_model       (nn.Module)   : Hair 영역 추출용 Segmentation Model
 # - eyes_vector          (NumPy array) : eyes (눈을 뜬 정도) 핵심 속성 값 변화 벡터
 # - mouth_vector         (NumPy array) : mouth (입을 벌린 정도) 핵심 속성 값 변화 벡터
 # - pose_vector          (NumPy array) : pose (고개 돌림) 핵심 속성 값 변화 벡터
@@ -60,8 +67,8 @@ kwargs_val = dict(trunc_psi=1.0, trunc_layers=0, randomize_noise=False)
 # - ombre_height         (float)       : 옴브레 염색 부분의 세로 길이 (0.0 - 1.0 범위)
 # - ombre_grad_height    (float)       : 옴브레 염색 부분의 그라데이션 부분의 세로 길이 비율 (0.0 - 1.0 범위)
 
-def generate_ombre_img(vectorfind_generator, eyes_vector, mouth_vector, pose_vector, code_w, save_dir, img_file_name, pms,
-                       color, ombre_height, ombre_grad_height):
+def generate_ombre_img(vectorfind_generator, hair_seg_model, eyes_vector, mouth_vector, pose_vector, code_w, save_dir,
+                       img_file_name, pms, color=0.0, ombre_height=0.3, ombre_grad_height=0.4):
 
     def generate_image_using_w(vectorfind_generator, w, trunc_psi=1.0, trunc_layers=0, randomize_noise=False, lod=None):
         with torch.no_grad():
@@ -81,9 +88,72 @@ def generate_ombre_img(vectorfind_generator, eyes_vector, mouth_vector, pose_vec
 
         images = generate_image_using_w(vectorfind_generator, code_w_)
 
-    save_image(os.path.join(save_dir, img_file_name), images[0])
+    ombre_image = apply_ombre(hair_seg_model, images[0], color, ombre_height, ombre_grad_height)
+    save_image(os.path.join(save_dir, img_file_name), ombre_image)
 
-    # TODO 옴브레 이미지 적용 구현
+
+# 옴브레 스타일 적용
+# Create Date : 2025.06.26
+# Last Update Date : -
+
+# Arguments:
+# - hair_seg_model    (nn.Module)   : Hair 영역 추출용 Segmentation Model
+# - original_image    (Numpy array) : 옴브레 스타일 적용 이전 이미지
+# - color             (float)       : 색상 값 (0.0 - 1.0 범위)
+# - ombre_height      (float)       : 옴브레 염색 부분의 세로 길이 (0.0 - 1.0 범위)
+# - ombre_grad_height (float)       : 옴브레 염색 부분의 그라데이션 부분의 세로 길이 비율 (0.0 - 1.0 범위)
+
+# Returns:
+# - ombre_image (Numpy array) : 옴브레 스타일 적용된 이미지
+
+def apply_ombre(hair_seg_model, original_image, color, ombre_height, ombre_grad_height):
+    original_image_tensor = seg_model_transform(original_image)
+
+    # load image & generate hair area map
+    hair_area_map = hair_seg_model(original_image_tensor.unsqueeze(0).cuda())
+    hair_area_map = hair_area_map.detach().cpu().numpy()
+    hair_area_map = hair_area_map[0, 0, :, :] * 255.0
+    hair_area_map = np.expand_dims(hair_area_map, axis=2)
+    hair_area_map = cv2.resize(hair_area_map, dsize=(IMAGE_RESOLUTION, IMAGE_RESOLUTION), interpolation=cv2.INTER_CUBIC)
+
+    hair_area_map = (hair_area_map - 0.75 * 255.0) * 4.0
+    hair_area_map = np.clip(hair_area_map, 0.0, 255.0)
+
+    # compute top & bottom y-axis of hair area
+    hair_top_y = None
+    hair_bottom_y = None
+
+    for y in range(IMAGE_RESOLUTION):
+        if max(hair_area_map[y]) >= 0.75 * 255:
+            if hair_top_y is None:
+                hair_top_y = y
+            hair_bottom_y = y
+
+    # decide area to apply ombre style
+    if hair_top_y is not None and hair_bottom_y is not None:
+        ombre_grad_top_y = int(hair_top_y + (hair_bottom_y - hair_top_y) * (1.0 - ombre_height))
+        ombre_grad_bottom_y = int(ombre_grad_top_y + (hair_bottom_y - ombre_grad_top_y) * ombre_grad_height)
+
+        weights = np.zeros(IMAGE_RESOLUTION)
+        weights[:ombre_grad_top_y] = 0.0
+        weights[ombre_grad_top_y:ombre_grad_bottom_y] = (
+                np.arange(ombre_grad_bottom_y - ombre_grad_top_y) / (ombre_grad_bottom_y - ombre_grad_top_y))
+        weights[ombre_grad_bottom_y:] = 1.0
+
+        hair_area_map = hair_area_map * weights[:, np.newaxis]
+
+    # apply ombre style
+    image_hsv = cv2.cvtColor(original_image, cv2.COLOR_BGR2HSV).astype(np.float32)
+    image_hsv[:, :, 0] = 30
+    image_hsv = image_hsv.astype(np.uint8)
+    converted_image = cv2.cvtColor(image_hsv, cv2.COLOR_HSV2BGR)
+    save_image('converted_image.jpg', converted_image)
+
+    # generate final ombre image
+    hair_area_map_ = np.expand_dims(hair_area_map, axis=2) / 255.0
+    ombre_image = (1.0 - hair_area_map_) * original_image + hair_area_map_ * converted_image
+
+    return ombre_image
 
 
 # 옴브레 염색 적용된 이미지 생성 (entrance 함수)
@@ -92,13 +162,16 @@ def generate_ombre_img(vectorfind_generator, eyes_vector, mouth_vector, pose_vec
 
 # Arguments:
 # - vectorfind_generator (nn.Module) : StyleGAN-VectorFind-v7 or v8 의 Generator
+# - hair_seg_model       (nn.Module) : Hair 영역 추출용 Segmentation Model
 # - vectorfind_ver       (str)       : StyleGAN-VectorFind 버전 ('v7' or 'v8')
 # - ohlora_no            (int)       : Oh-LoRA 이미지 번호 ('v7'의 경우 127, 672, 709, ...)
 # - color                (float)     : 색상 값 (0.0 - 1.0 범위)
 # - ombre_height         (float)     : 옴브레 염색 부분의 세로 길이 (0.0 - 1.0 범위)
 # - ombre_grad_height    (float)     : 옴브레 염색 부분의 그라데이션 부분의 세로 길이 비율 (0.0 - 1.0 범위)
 
-def generate_ombre_image(vectorfind_generator, vectorfind_ver, ohlora_no, color, ombre_height, ombre_grad_height):
+def generate_ombre_image(vectorfind_generator, hair_seg_model, vectorfind_ver, ohlora_no,
+                         color, ombre_height, ombre_grad_height):
+
     raise NotImplementedError
 
 
@@ -108,11 +181,12 @@ def generate_ombre_image(vectorfind_generator, vectorfind_ver, ohlora_no, color,
 
 # Arguments:
 # - vectorfind_v7_generator (nn.Module)         : StyleGAN-VectorFind-v7 의 Generator
+# - hair_seg_model          (nn.Module)         : Hair 영역 추출용 Segmentation Model
 # - eyes_vectors            (dict(NumPy Array)) : eyes (눈을 뜬 정도) 속성값을 변화시키는 벡터 정보 (각 그룹 별)
 # - mouth_vectors           (dict(NumPy Array)) : mouth (입을 벌린 정도) 속성값을 변화시키는 벡터 정보 (각 그룹 별)
 # - pose_vectors            (dict(NumPy Array)) : pose (고개 돌림) 속성값을 변화시키는 벡터 정보 (각 그룹 별)
 
-def generate_ombre_image_using_v7(vectorfind_v7_generator, eyes_vectors, mouth_vectors, pose_vectors):
+def generate_ombre_image_using_v7(vectorfind_v7_generator, hair_seg_model, eyes_vectors, mouth_vectors, pose_vectors):
     n_vector_cnt = len(eyes_vectors['hhh'])  # equal to pre-defined SVMS_PER_EACH_PROPERTY value
 
     ohlora_z_vector_csv_path = f'{PROJECT_DIR_PATH}/stylegan/stylegan_vectorfind_v7/ohlora_z_vectors.csv'
@@ -155,8 +229,9 @@ def generate_ombre_image_using_v7(vectorfind_v7_generator, eyes_vectors, mouth_v
                 img_file_name = f'case_{i:03d}_{vi:03d}_pm_{pm_idx:03d}.jpg'
                 pms = {'eyes': eyes_pm_order[pm_idx], 'mouth': mouth_pm_order[pm_idx], 'pose': pose_pm_order[pm_idx]}
 
-                generate_ombre_img(vectorfind_v7_generator, eyes_vector, mouth_vector, pose_vector, code_w, save_dir,
-                                   img_file_name, pms, color=0.0, ombre_height=0.3, ombre_grad_height=0.4)
+                generate_ombre_img(vectorfind_v7_generator, hair_seg_model, eyes_vector, mouth_vector, pose_vector,
+                                   code_w, save_dir, img_file_name, pms,
+                                   color=0.0, ombre_height=0.3, ombre_grad_height=0.4)
 
 
 # StyleGAN-VectorFind-v7 옴브레 염색 적용 이미지 생성 테스트 (모델 로딩을 포함한 전 과정)
@@ -164,9 +239,9 @@ def generate_ombre_image_using_v7(vectorfind_v7_generator, eyes_vectors, mouth_v
 # Last Update Date : -
 
 # Arguments:
-# - 없음
+# - hair_seg_model (nn.Module) : Hair 영역 추출용 Segmentation Model
 
-def generate_ombre_image_using_v7_all_process():
+def generate_ombre_image_using_v7_all_process(hair_seg_model):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f'device for inferencing StyleGAN-FineTune-v1 : {device}')
 
@@ -185,6 +260,7 @@ def generate_ombre_image_using_v7_all_process():
     vectorfind_v7_generator.to(device)
 
     generate_ombre_image_using_v7(vectorfind_v7_generator,
+                                  hair_seg_model,
                                   eyes_vectors,
                                   mouth_vectors,
                                   pose_vectors)
@@ -196,11 +272,12 @@ def generate_ombre_image_using_v7_all_process():
 
 # Arguments:
 # - vectorfind_v8_generator (nn.Module)         : StyleGAN-VectorFind-v8 의 Generator
+# - hair_seg_model          (nn.Module)         : Hair 영역 추출용 Segmentation Model
 # - eyes_vectors            (dict(NumPy Array)) : eyes (눈을 뜬 정도) 속성값을 변화시키는 벡터 정보 (각 그룹 별)
 # - mouth_vectors           (dict(NumPy Array)) : mouth (입을 벌린 정도) 속성값을 변화시키는 벡터 정보 (각 그룹 별)
 # - pose_vectors            (dict(NumPy Array)) : pose (고개 돌림) 속성값을 변화시키는 벡터 정보 (각 그룹 별)
 
-def generate_ombre_image_using_v8(vectorfind_v8_generator, eyes_vectors, mouth_vectors, pose_vectors):
+def generate_ombre_image_using_v8(vectorfind_v8_generator, hair_seg_model, eyes_vectors, mouth_vectors, pose_vectors):
     n_vector_cnt = len(eyes_vectors['hhhh'])  # equal to pre-defined SVMS_PER_EACH_PROPERTY value
 
     ohlora_z_vector_csv_path = f'{PROJECT_DIR_PATH}/stylegan/stylegan_vectorfind_v8/ohlora_z_vectors.csv'
@@ -243,8 +320,9 @@ def generate_ombre_image_using_v8(vectorfind_v8_generator, eyes_vectors, mouth_v
                 img_file_name = f'case_{i:03d}_{vi:03d}_pm_{pm_idx:03d}.jpg'
                 pms = {'eyes': eyes_pm_order[pm_idx], 'mouth': mouth_pm_order[pm_idx], 'pose': pose_pm_order[pm_idx]}
 
-                generate_ombre_img(vectorfind_v8_generator, eyes_vector, mouth_vector, pose_vector, code_w, save_dir,
-                                   img_file_name, pms, color=0.0, ombre_height=0.3, ombre_grad_height=0.4)
+                generate_ombre_img(vectorfind_v8_generator, hair_seg_model, eyes_vector, mouth_vector, pose_vector,
+                                   code_w, save_dir, img_file_name, pms,
+                                   color=0.0, ombre_height=0.3, ombre_grad_height=0.4)
 
 
 # StyleGAN-VectorFind-v8 옴브레 염색 적용 이미지 생성 테스트 (모델 로딩을 포함한 전 과정)
@@ -252,9 +330,9 @@ def generate_ombre_image_using_v8(vectorfind_v8_generator, eyes_vectors, mouth_v
 # Last Update Date : -
 
 # Arguments:
-# - 없음
+# - hair_seg_model (nn.Module) : Hair 영역 추출용 Segmentation Model
 
-def generate_ombre_image_using_v8_all_process():
+def generate_ombre_image_using_v8_all_process(hair_seg_model):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f'device for inferencing StyleGAN-FineTune-v8 : {device}')
 
@@ -273,6 +351,7 @@ def generate_ombre_image_using_v8_all_process():
     finetune_v8_generator.to(device)
 
     generate_ombre_image_using_v8(finetune_v8_generator,
+                                  hair_seg_model,
                                   eyes_vectors,
                                   mouth_vectors,
                                   pose_vectors)
--- 2025_06_24_OhLoRA_v4/ombre/run_all.py
+++ 2025_06_24_OhLoRA_v4/ombre/run_all.py
@@ -3,10 +3,48 @@ from generate_ombre_images import generate_ombre_image_using_v7_all_process, gen
 from generate_gif import generate_gif
 from generate_opencv_screen import create_opencv_screen
 
+import torch
+
+import os
+import sys
+PROJECT_DIR_PATH = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+sys.path.append(PROJECT_DIR_PATH)
+
+from segmentation.seg_model_ohlora_v4.model import SegModelForOhLoRAV4
+
+
+# Oh-LoRA v4 용 Hair 영역 추출 Segmentation 모델 로딩
+# Create Date : 2025.06.26
+# Last Update Date : -
+
+# Arguments:
+# - device (Device) : CUDA or CPU device
+
+# Returns:
+# - hair_seg_model (OrderedDict) : Oh-LoRA v4 용 Hair 영역 추출 Segmentation 모델
+
+def load_existing_hair_seg_model(device):
+    hair_seg_model = SegModelForOhLoRAV4()
+
+    # load generator state dict
+    hair_seg_model_path = f'{PROJECT_DIR_PATH}/segmentation/models/segmentation_model_ohlora_v4.pth'
+    hair_seg_model_state_dict = torch.load(hair_seg_model_path, map_location=device, weights_only=True)
+    hair_seg_model.load_state_dict(hair_seg_model_state_dict)
+
+    hair_seg_model.to(device)
+    return hair_seg_model
+
 
 if __name__ == '__main__':
-    generate_ombre_image_using_v7_all_process()
-    generate_ombre_image_using_v8_all_process()
+
+    # check device
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f'device for training : {device}')
+
+    # load segmentation model
+    hair_seg_model = load_existing_hair_seg_model(device)
+    generate_ombre_image_using_v7_all_process(hair_seg_model)
+    generate_ombre_image_using_v8_all_process(hair_seg_model)
 
     v7_test_ohlora_nos = [672, 1277, 1836, 1918, 2137]
     v8_test_ohlora_nos = [83, 194, 1180, 1313, 1996]
