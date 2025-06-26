9538b0a843ade4eb1772c2bacd3eefed59b10b4c
wannabesuperteur <iwant_optimi@naver.com>
2025-06-26 16:52:01 +0900

    ADD: gif generation (1)

--- 2025_06_24_OhLoRA_v4/ombre/generate_gif.py
+++ 2025_06_24_OhLoRA_v4/ombre/generate_gif.py
@@ -1,4 +1,48 @@
 from generate_ombre_images import generate_ombre_image
+from stylegan.run_stylegan_vectorfind_v7 import get_property_change_vectors as get_property_change_vectors_v7
+from stylegan.run_stylegan_vectorfind_v8 import get_property_change_vectors as get_property_change_vectors_v8
+
+import imageio
+
+
+# GIF 이미지 생성
+# Create Date : 2025.06.26
+# Last Update Date : -
+
+# Arguments:
+# - vectorfind_generator   (nn.Module)         : StyleGAN-VectorFind-v7 or v8 의 Generator
+# - hair_seg_model         (nn.Module)         : Hair 영역 추출용 Segmentation Model
+# - vectorfind_ver         (str)               : StyleGAN-VectorFind 버전 ('v7' or 'v8')
+# - ohlora_no              (int)               : Oh-LoRA 이미지 번호 ('v7'의 경우 127, 672, 709, ...)
+# - color_list             (list(float))       : 색상 값 (0.0 - 1.0 범위) 의 list
+# - ombre_height_list      (list(float))       : 옴브레 염색 부분의 세로 길이 (0.0 - 1.0 범위) 의 list
+# - ombre_grad_height_list (list(float))       : 옴브레 염색 부분의 그라데이션 부분의 세로 길이 비율 (0.0 - 1.0 범위) 의 list
+# - pms_list               (dict(list(float))) : eyes, mouth, pose 핵심 속성 값 변화 벡터를 latent code 에 더하거나 빼기 위한
+#                                                가중치의 list
+#                                                {'eyes': list(float), 'mouth': list(float), 'pose': list(float)}
+# - gif_save_path          (str)               : GIF 이미지의 저장 경로
+# - duration               (float)             : 프레임 당 시간 (초)
+
+def generate_gif(vectorfind_generator, hair_seg_model, vectorfind_ver, ohlora_no,
+                 color_list, ombre_height_list, ombre_grad_height_list, pms_list, gif_save_path, duration=0.1):
+
+    print(f'generating GIF image for {vectorfind_ver} / {ohlora_no} ...')
+    eyes_vectors, mouth_vectors, pose_vectors = get_property_score_change_vectors(vectorfind_ver)
+    ohlora_images = []
+    n_frames = len(color_list)
+
+    for idx in range(n_frames):
+        color = color_list[idx]
+        ombre_height = ombre_height_list[idx]
+        ombre_grad_height = ombre_grad_height_list[idx]
+        pms = {'eyes': pms_list['eyes'][idx], 'mouth': pms_list['mouth'][idx], 'pose': pms_list['pose'][idx]}
+
+        ohlora_image = generate_ombre_image(vectorfind_generator, hair_seg_model,
+                                            eyes_vectors, mouth_vectors, pose_vectors,
+                                            vectorfind_ver, ohlora_no, color, ombre_height, ombre_grad_height, pms)
+        ohlora_images.append(ohlora_image)
+
+    imageio.mimsave(gif_save_path, ohlora_images, duration=duration)
 
 
 # GIF 이미지 생성
@@ -7,7 +51,17 @@ from generate_ombre_images import generate_ombre_image
 
 # Arguments:
 # - vectorfind_ver (str) : StyleGAN-VectorFind 버전 ('v7' or 'v8')
-# - ohlora_no      (int) : Oh-LoRA 이미지 번호 ('v7'의 경우 127, 672, 709, ...)
 
-def generate_gif(vectorfind_ver, ohlora_no):
-    raise NotImplementedError
+# Returns:
+# - eyes_vectors  (dict(NumPy Array)) : eyes (눈을 뜬 정도) 속성값을 변화시키는 벡터 정보 (각 그룹 별)
+# - mouth_vectors (dict(NumPy Array)) : mouth (입을 벌린 정도) 속성값을 변화시키는 벡터 정보 (각 그룹 별)
+# - pose_vectors  (dict(NumPy Array)) : pose (고개 돌림) 속성값을 변화시키는 벡터 정보 (각 그룹 별)
+
+def get_property_score_change_vectors(vectorfind_ver):
+    if vectorfind_ver == 'v7':
+        eyes_vectors, mouth_vectors, pose_vectors = get_property_change_vectors_v7()
+    else:  # v8
+        eyes_vectors, mouth_vectors, pose_vectors = get_property_change_vectors_v8(vectorfind_ver)
+
+    return eyes_vectors, mouth_vectors, pose_vectors
+
--- 2025_06_24_OhLoRA_v4/ombre/run_all.py
+++ 2025_06_24_OhLoRA_v4/ombre/run_all.py
@@ -11,6 +11,10 @@ PROJECT_DIR_PATH = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 sys.path.append(PROJECT_DIR_PATH)
 
 from segmentation.seg_model_ohlora_v4.model import SegModelForOhLoRAV4
+from stylegan.common import load_existing_stylegan_vectorfind_v7, load_existing_stylegan_vectorfind_v8
+import stylegan.stylegan_common.stylegan_generator as gen
+
+IMAGE_RESOLUTION = 256
 
 
 # Oh-LoRA v4 용 Hair 영역 추출 Segmentation 모델 로딩
@@ -43,18 +47,58 @@ if __name__ == '__main__':
 
     # load segmentation model
     hair_seg_model = load_existing_hair_seg_model(device)
-    generate_ombre_image_using_v7_all_process(hair_seg_model)
-    generate_ombre_image_using_v8_all_process(hair_seg_model)
+#    generate_ombre_image_using_v7_all_process(hair_seg_model)
+#    generate_ombre_image_using_v8_all_process(hair_seg_model)
+
+    # load StyleGAN-VectorFind-v7 and v8
+    stylegan_vectorfind_v7_generator = gen.StyleGANGeneratorForV6(resolution=IMAGE_RESOLUTION)
+    stylegan_vectorfind_v7_state_dict = load_existing_stylegan_vectorfind_v7(device)
+    stylegan_vectorfind_v7_generator.load_state_dict(stylegan_vectorfind_v7_state_dict)
+    stylegan_vectorfind_v7_generator.to(device)
+
+    stylegan_vectorfind_v8_generator = gen.StyleGANGenerator(resolution=IMAGE_RESOLUTION)
+    stylegan_vectorfind_v8_state_dict = load_existing_stylegan_vectorfind_v8(device)
+    stylegan_vectorfind_v8_generator.load_state_dict(stylegan_vectorfind_v8_state_dict)
+    stylegan_vectorfind_v8_generator.to(device)
 
     v7_test_ohlora_nos = [672, 1277, 1836, 1918, 2137]
     v8_test_ohlora_nos = [83, 194, 1180, 1313, 1996]
 
     # GIF 생성 테스트
+    color_list = [0.025 * x for x in range(40)]
+    ombre_height_list = [0.1 + 0.01 * x for x in range(40)]
+    ombre_grad_height_list = [0.05 + 0.015 * x for x in range(40)]
+    pms_list = {
+        'eyes': [-1.2 + 0.12 * x for x in range(20)] + [1.2 - 0.12 * x for x in range(20)],
+        'mouth': [1.2 - 0.06 * x for x in range(40)],
+        'pose': [1.2 - 0.09 * x for x in range(20)] + [-0.6 for _ in range(20)]
+    }
+
+    gif_save_dir_path = f'{PROJECT_DIR_PATH}/ombre/test_gif'
+    os.makedirs(f'{gif_save_dir_path}/v7', exist_ok=True)
+    os.makedirs(f'{gif_save_dir_path}/v8', exist_ok=True)
+
     for v7_test_ohlora_no in v7_test_ohlora_nos:
-        generate_gif(vectorfind_ver='v7', ohlora_no=v7_test_ohlora_no)
+        generate_gif(vectorfind_generator=stylegan_vectorfind_v7_generator,
+                     hair_seg_model=hair_seg_model,
+                     vectorfind_ver='v7',
+                     ohlora_no=v7_test_ohlora_no,
+                     color_list=color_list,
+                     ombre_height_list=ombre_height_list,
+                     ombre_grad_height_list=ombre_grad_height_list,
+                     pms_list=pms_list,
+                     gif_save_path=f'{gif_save_dir_path}/v7/{v7_test_ohlora_no}_gif.gif')
 
     for v8_test_ohlora_no in v8_test_ohlora_nos:
-        generate_gif(vectorfind_ver='v8', ohlora_no=v8_test_ohlora_no)
+        generate_gif(vectorfind_generator=stylegan_vectorfind_v8_generator,
+                     hair_seg_model=hair_seg_model,
+                     vectorfind_ver='v8',
+                     ohlora_no=v8_test_ohlora_no,
+                     color_list=color_list,
+                     ombre_height_list=ombre_height_list,
+                     ombre_grad_height_list=ombre_grad_height_list,
+                     pms_list=pms_list,
+                     gif_save_path=f'{gif_save_dir_path}/v8/{v8_test_ohlora_no}_gif.gif')
 
     # OpenCV 움직이는 화면 생성 테스트
     for v7_test_ohlora_no in v7_test_ohlora_nos:
