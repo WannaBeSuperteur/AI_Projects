9eb4b7016063b00e52160d35f8394bf2ed39be6c
wannabesuperteur <iwant_optimi@naver.com>
2025-06-28 08:28:48 +0900

    ADD: Oh-LoRA v4 ethics dataset

---
+++ 2025_06_24_OhLoRA_v4/llm/ethics_mechanism/convert_xlsx_to_csv.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+if __name__ == '__main__':
+    xlsx = pd.read_excel('train_test_dataset.xlsx')
+    xlsx.rename(columns={'Unnamed: 0': 'idx'}, inplace=True)
+    xlsx.to_csv('train_test_dataset.csv', index=False)
