7f9ad4b65e6940d30582427bb6e3a2bd9bc049d8
wannabesuperteur <iwant_optimi@naver.com>
2025-06-29 15:30:47 +0900

    UPDATE: ombre-style applied display function

--- 2025_06_24_OhLoRA_v4/final_product/run_display.py
+++ 2025_06_24_OhLoRA_v4/final_product/run_display.py
@@ -1,6 +1,21 @@
 
-from generate_ohlora_image import generate_images
 import cv2
+import numpy as np
+import torch
+
+import os
+import sys
+
+PROJECT_DIR_PATH = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+sys.path.append(PROJECT_DIR_PATH)
+
+from ombre.generate_ombre_images import generate_ombre_img
+
+
+ORIGINAL_HIDDEN_DIMS_Z = 512
+ORIGINALLY_PROPERTY_DIMS_V7 = 3  # 원래 property (eyes, mouth, pose) 목적으로 사용된 dimension 값
+ORIGINALLY_PROPERTY_DIMS_V8 = 7  # 원래 property (eyes, hair_color, hair_length, mouth, pose,
+                                 #               background_mean, background_std) 목적으로 사용된 dimension 값
 
 
 # Oh-LoRA (오로라) 이미지 생성 및 표시
@@ -21,12 +36,32 @@ import cv2
 # - 직접 반환되는 값 없음
 # - final_product/ohlora.png 경로에 오로라 이미지 생성 및 화면에 display
 
-def generate_and_show_ohlora_image(vectorfind_generator, ohlora_z_vector, eyes_vector, mouth_vector, pose_vector,
-                                   eyes_score, mouth_score, pose_score):
+def generate_and_show_ohlora_image(vectorfind_generator, hair_seg_model, vectorfind_ver,
+                                   ohlora_z_vector, eyes_vector, mouth_vector, pose_vector,
+                                   eyes_score, mouth_score, pose_score,
+                                   color, ombre_height, ombre_grad_height):
 
-    ohlora_image_to_display = generate_images(vectorfind_generator, ohlora_z_vector,
-                                              eyes_vector, mouth_vector, pose_vector,
-                                              eyes_pm=eyes_score, mouth_pm=mouth_score, pose_pm=pose_score)
+    code_part1s_np = np.zeros((1, ORIGINAL_HIDDEN_DIMS_Z))
+    if vectorfind_ver == 'v7':
+        code_part2s_np = np.zeros((1, ORIGINALLY_PROPERTY_DIMS_V7))
+    else:  # v8
+        code_part2s_np = np.zeros((1, ORIGINALLY_PROPERTY_DIMS_V8))
 
-    cv2.imshow('Oh-LoRA', ohlora_image_to_display[:, :, ::-1])
+    # image generation
+    code_part1s_np[0] = ohlora_z_vector[:ORIGINAL_HIDDEN_DIMS_Z]
+    code_part2s_np[0] = ohlora_z_vector[ORIGINAL_HIDDEN_DIMS_Z:]
+    code_part1 = torch.tensor(code_part1s_np[0]).unsqueeze(0).to(torch.float32)  # 512
+    code_part2 = torch.tensor(code_part2s_np[0]).unsqueeze(0).to(torch.float32)  # 3 (VectorFind-v7), 7 (VectorFind-v8)
+
+    pms = {'eyes': eyes_score, 'mouth': mouth_score, 'pose': pose_score}
+
+    with torch.no_grad():
+        code_w = vectorfind_generator.mapping(code_part1.cuda(), code_part2.cuda())['w'].detach().cpu()
+
+        ombre_image = generate_ombre_img(vectorfind_generator, hair_seg_model, eyes_vector, mouth_vector, pose_vector,
+                                         code_w, save_dir=None, img_file_name=None, pms=pms,
+                                         color=color, ombre_height=ombre_height, ombre_grad_height=ombre_grad_height,
+                                         instruct='return')
+
+    cv2.imshow('Oh-LoRA', ombre_image[:, :, ::-1])
     _ = cv2.waitKey(1)
