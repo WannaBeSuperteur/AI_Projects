891ef8ef403f0d952d05a9a10611625bce4fad23
wannabesuperteur <iwant_optimi@naver.com>
2025-07-04 22:41:15 +0900

    ADD: Final SFT dataset

--- 2025_07_02_OhLoRA_ML_Tutor/ai_qna/fine_tuning_dataset/convert_xlsx_to_csv.py
+++ 2025_07_02_OhLoRA_ML_Tutor/ai_qna/fine_tuning_dataset/convert_xlsx_to_csv.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 if __name__ == '__main__':
-    xlsx = pd.read_excel('SFT_with_RAG_concept.xlsx')
+    xlsx = pd.read_excel('SFT_final.xlsx')
     xlsx.rename(columns={'Unnamed: 0': 'idx'}, inplace=True)
-    xlsx.to_csv('SFT_with_RAG_concept.csv', index=True)
+    xlsx.to_csv('SFT_final.csv', index=True)
