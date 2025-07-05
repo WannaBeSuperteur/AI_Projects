8067564ea038556a000b739b71324c8b8375a3ba
wannabesuperteur <iwant_optimi@naver.com>
2025-07-05 17:22:15 +0900

    ADD: S-BERT train & test dataset (3)

--- 2025_07_02_OhLoRA_ML_Tutor/ai_qna/rag_sbert/dataset/convert_xlsx_to_csv.py
+++ 2025_07_02_OhLoRA_ML_Tutor/ai_qna/rag_sbert/dataset/convert_xlsx_to_csv.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 if __name__ == '__main__':
-    xlsx = pd.read_excel('train_test_dataset.xlsx')
+    xlsx = pd.read_excel('train_test_dataset_new.xlsx')
     xlsx.rename(columns={'Unnamed: 0': 'idx'}, inplace=True)
-    xlsx.to_csv('train_test_dataset.csv', index=True)
+    xlsx.to_csv('train_test_dataset_new.csv', index=True)
