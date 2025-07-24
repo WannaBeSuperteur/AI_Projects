a3aa0ab644c7aa37bfe827be29aa9bd625dcdea4
wannabesuperteur <iwant_optimi@naver.com>
2025-07-24 18:21:26 +0900

    ADD: ML interview S-BERT train dataset

--- 2025_07_02_OhLoRA_ML_Tutor/ai_interview/dataset/extract_sbert_dataset.py
+++ 2025_07_02_OhLoRA_ML_Tutor/ai_interview/dataset/extract_sbert_dataset.py
@@ -1,21 +1,31 @@
 import pandas as pd
 
 
-if __name__ == '__main__':
+# S-BERT 학습 / 검증 / 테스트 데이터셋 추출
+# Create Date : 2025.07.24
+# Last Update Date : -
+
+# Arguments:
+# - data_type (str) : 학습 ('train') or 검증/테스트 ('valid/test')
+
+# Returns:
+# - dataset_df_answer_type   (Pandas DataFrame) : S-BERT 데이터셋 ("현재 질문 + 사용자 답변" & "사용자가 성공한 답변" 비교)
+# - dataset_df_next_question (Pandas DataFrame) : S-BERT 데이터셋 ("현재 질문 + 사용자 답변" & "다음 질문" 비교)
+
+def extract_sbert_dataset(data_type):
     all_data = pd.read_csv('all_train_and_test_data.csv')
-    all_data_train = all_data[all_data['data_type'] == 'train']
-    len_train = len(all_data_train)
+    all_data_with_type = all_data[all_data['data_type'] == 'train']
 
     # collect output part
-    output_answer_types = sorted(list(set(all_data_train['output_answered'].dropna().tolist())))
-    next_questions = sorted(list(set(all_data_train['output_next_question'].dropna().tolist())))
+    output_answer_types = sorted(list(set(all_data_with_type['output_answered'].dropna().tolist())))
+    next_questions = sorted(list(set(all_data_with_type['output_next_question'].dropna().tolist())))
 
     # collect input part (question & user input)
     current_question_and_user_input = []
     input_to_output_answer_type = {}
     input_to_next_question = {}
 
-    for _, row in all_data_train.iterrows():
+    for _, row in all_data_with_type.iterrows():
         current_question = row['current_question']
         user_input = row['user_input']
 
@@ -51,5 +61,15 @@ if __name__ == '__main__':
             else:
                 next_questions_sbert_train_dataset['similarity'].append(0.0)
 
-    print(pd.DataFrame(output_answer_sbert_train_dataset))
-    print(pd.DataFrame(next_questions_sbert_train_dataset))
+    dataset_df_answer_type = pd.DataFrame(output_answer_sbert_train_dataset)
+    dataset_df_next_question = pd.DataFrame(next_questions_sbert_train_dataset)
+
+    return dataset_df_answer_type, dataset_df_next_question
+
+
+if __name__ == '__main__':
+    dataset_df_answer_type, dataset_df_next_question = extract_sbert_dataset('train')
+    dataset_df_answer_type.to_csv('dataset_df_answer_type_train.csv', index=False)
+    dataset_df_next_question.to_csv('dataset_df_next_question_train.csv', index=False)
+
+
