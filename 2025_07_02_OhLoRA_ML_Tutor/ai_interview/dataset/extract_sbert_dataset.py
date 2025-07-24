780da8e867f340aa81042de2d88383994e28b0cc
wannabesuperteur <iwant_optimi@naver.com>
2025-07-24 21:53:57 +0900

    UPDATE: include 'remaining answer' for next question S-BERT dataset

--- 2025_07_02_OhLoRA_ML_Tutor/ai_interview/dataset/extract_sbert_dataset.py
+++ 2025_07_02_OhLoRA_ML_Tutor/ai_interview/dataset/extract_sbert_dataset.py
@@ -10,11 +10,11 @@ import pandas as pd
 
 # Returns:
 # - dataset_df_answer_type   (Pandas DataFrame) : S-BERT 데이터셋 ("현재 질문 + 사용자 답변" & "사용자가 성공한 답변" 비교)
-# - dataset_df_next_question (Pandas DataFrame) : S-BERT 데이터셋 ("현재 질문 + 사용자 답변" & "다음 질문" 비교)
+# - dataset_df_next_question (Pandas DataFrame) : S-BERT 데이터셋 ("현재 질문 + 남은 답변 + 사용자 답변" & "다음 질문" 비교)
 
 def extract_sbert_dataset(data_type):
     all_data = pd.read_csv('all_train_and_test_data.csv')
-    all_data_with_type = all_data[all_data['data_type'] == 'train']
+    all_data_with_type = all_data[all_data['data_type'] == data_type]
 
     # collect output part
     output_answer_types = sorted(list(set(all_data_with_type['output_answered'].dropna().tolist())))
@@ -22,21 +22,28 @@ def extract_sbert_dataset(data_type):
 
     # collect input part (question & user input)
     current_question_and_user_input = []
+    current_question_and_user_input_with_remaining_answer_info = []
+
     input_to_output_answer_type = {}
     input_to_next_question = {}
 
     for _, row in all_data_with_type.iterrows():
         current_question = row['current_question']
         user_input = row['user_input']
+        remaining_answer = row['current_remaining_user_answer']
 
         if not pd.isna(current_question) and not pd.isna(user_input):
-            input_sentence = f'{current_question} -> {user_input}'
             output_answer_type = row['output_answered']
             next_question = row['output_next_question']
 
+            input_sentence = f'{current_question} -> {user_input}'
             current_question_and_user_input.append(input_sentence)
             input_to_output_answer_type[input_sentence] = output_answer_type
-            input_to_next_question[input_sentence] = next_question
+
+            if not pd.isna(remaining_answer):
+                input_with_remaining_answer_info = f'{current_question} -> {user_input} (남은 답변: {remaining_answer})'
+                current_question_and_user_input_with_remaining_answer_info.append(input_with_remaining_answer_info)
+                input_to_next_question[input_with_remaining_answer_info] = next_question
 
     # combine input part & output part -> for S-BERT training
     output_answer_sbert_train_dataset = {'input_part': [], 'output_answer': [], 'similarity': []}
@@ -52,6 +59,7 @@ def extract_sbert_dataset(data_type):
             else:
                 output_answer_sbert_train_dataset['similarity'].append(0.0)
 
+    for input_part in current_question_and_user_input_with_remaining_answer_info:
         for next_question in next_questions:
             next_questions_sbert_train_dataset['input_part'].append(input_part)
             next_questions_sbert_train_dataset['next_question'].append(next_question)
