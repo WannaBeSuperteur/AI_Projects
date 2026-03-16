e70ee2e1fd8b8789e0b371cecb50171ed0876242
wannabesuperteur <iwant_optimi@naver.com>
2026-03-16 22:14:57 +0900

    UPDATE: simplify Auto-Encoder architecture

--- 2025_10_06_OhLoRA_HP_Battle/hidden_representation/auto_encoder.py
+++ 2025_10_06_OhLoRA_HP_Battle/hidden_representation/auto_encoder.py
@@ -9,22 +9,22 @@ class AutoEncoderEncoder_1_28_28(nn.Module):
 
         # Conv Layers
         self.conv1 = nn.Sequential(
-            nn.Conv2d(1, 64, kernel_size=4, stride=2, padding=1, padding_mode='reflect'),
+            nn.Conv2d(1, 32, kernel_size=4, stride=2, padding=1, padding_mode='reflect'),
             nn.LeakyReLU(),
             nn.Dropout2d(0.05)
         )
         self.conv2 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, padding_mode='reflect'),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1, padding_mode='reflect'),
             nn.LeakyReLU(),
             nn.Dropout2d(0.05)
         )
         self.conv3 = nn.Sequential(
-            nn.Conv2d(128, 256, kernel_size=3, stride=1),
+            nn.Conv2d(64, 128, kernel_size=3, stride=1),
             nn.LeakyReLU(),
             nn.Dropout2d(0.05)
         )
         self.conv4 = nn.Sequential(
-            nn.Conv2d(256, 512, kernel_size=3, stride=1),
+            nn.Conv2d(128, 256, kernel_size=3, stride=1),
             nn.LeakyReLU(),
             nn.Dropout2d(0.05)
         )
@@ -46,19 +46,19 @@ class AutoEncoderDecoder_1_28_28(nn.Module):
 
         # DeConv Layers
         self.decoder_deconv1 = nn.Sequential(
-            nn.ConvTranspose2d(512, 256, kernel_size=3, stride=1),
-            nn.LeakyReLU()
-        )
-        self.decoder_deconv2 = nn.Sequential(
             nn.ConvTranspose2d(256, 128, kernel_size=3, stride=1),
             nn.LeakyReLU()
         )
+        self.decoder_deconv2 = nn.Sequential(
+            nn.ConvTranspose2d(128, 64, kernel_size=3, stride=1),
+            nn.LeakyReLU()
+        )
         self.decoder_deconv3 = nn.Sequential(
-            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1, padding_mode='zeros'),
+            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1, padding_mode='zeros'),
             nn.LeakyReLU()
         )
         self.decoder_deconv4 = nn.Sequential(
-            nn.ConvTranspose2d(64, 1, kernel_size=4, stride=2, padding=1, padding_mode='zeros'),
+            nn.ConvTranspose2d(32, 1, kernel_size=4, stride=2, padding=1, padding_mode='zeros'),
             nn.LeakyReLU()
         )
 
@@ -79,22 +79,22 @@ class AutoEncoderEncoder_3_32_32(nn.Module):
 
         # Conv Layers
         self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1, padding_mode='reflect'),
+            nn.Conv2d(3, 32, kernel_size=4, stride=2, padding=1, padding_mode='reflect'),
             nn.LeakyReLU(),
             nn.Dropout2d(0.05)
         )
         self.conv2 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, padding_mode='reflect'),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1, padding_mode='reflect'),
             nn.LeakyReLU(),
             nn.Dropout2d(0.05)
         )
         self.conv3 = nn.Sequential(
-            nn.Conv2d(128, 256, kernel_size=3, stride=1),
+            nn.Conv2d(64, 128, kernel_size=3, stride=1),
             nn.LeakyReLU(),
             nn.Dropout2d(0.05)
         )
         self.conv4 = nn.Sequential(
-            nn.Conv2d(256, 512, kernel_size=3, stride=1),
+            nn.Conv2d(128, 256, kernel_size=3, stride=1),
             nn.LeakyReLU(),
             nn.Dropout2d(0.05)
         )
@@ -116,19 +116,19 @@ class AutoEncoderDecoder_3_32_32(nn.Module):
 
         # DeConv Layers
         self.decoder_deconv1 = nn.Sequential(
-            nn.ConvTranspose2d(512, 256, kernel_size=3, stride=1),
-            nn.LeakyReLU()
-        )
-        self.decoder_deconv2 = nn.Sequential(
             nn.ConvTranspose2d(256, 128, kernel_size=3, stride=1),
             nn.LeakyReLU()
         )
+        self.decoder_deconv2 = nn.Sequential(
+            nn.ConvTranspose2d(128, 64, kernel_size=3, stride=1),
+            nn.LeakyReLU()
+        )
         self.decoder_deconv3 = nn.Sequential(
-            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1, padding_mode='zeros'),
+            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1, padding_mode='zeros'),
             nn.LeakyReLU()
         )
         self.decoder_deconv4 = nn.Sequential(
-            nn.ConvTranspose2d(64, 3, kernel_size=4, stride=2, padding=1, padding_mode='zeros'),
+            nn.ConvTranspose2d(32, 3, kernel_size=4, stride=2, padding=1, padding_mode='zeros'),
             nn.LeakyReLU()
         )
 
