f1f45809d9ee391fa48e57b96d155807ef9e750a
wannabesuperteur <iwant_optimi@naver.com>
2026-03-17 23:37:13 +0900

    ADD: Auto-Encoder test basic flow

--- 2025_10_06_OhLoRA_HP_Battle/hidden_representation/test_ae.py
+++ 2025_10_06_OhLoRA_HP_Battle/hidden_representation/test_ae.py
@@ -31,6 +31,7 @@ def load_ae_encoder(dataset_name):
 
     model_path = f'{PROJECT_DIR_PATH}/models/ae_encoder_{dataset_name}.pt'
     model.load_state_dict(torch.load(model_path, map_location=device, weights_only=True))
+    model.device = device
     return model
 
 
@@ -57,6 +58,42 @@ def load_ae_entire_model(dataset_name):
 
     model_path = f'{PROJECT_DIR_PATH}/models/ae_model_{dataset_name}.pt'
     model.load_state_dict(torch.load(model_path, map_location=device, weights_only=True))
+    model.device = device
     return model
 
 
+# Auto-Encoder 모델의 Re-construction 성능 테스트 (시각적 결과)
+# Create Date : 2026.03.18
+# Last Update Date : -
+
+# Arguments:
+# - dataset_name    (str)       : 데이터셋 이름 ('cifar_10', 'fashion_mnist' or 'mnist')
+# - ae_entire_model (nn.Module) : Auto-Encoder 모델 전체 (Encoder + Decoder)
+
+def run_reconstruction_test(dataset_name, ae_entire_model):
+    raise NotImplementedError
+
+
+# Auto-Encoder 모델의 Hidden Representation 테스트 (가까운 이미지 & 거리가 큰 이미지)
+# Create Date : 2026.03.18
+# Last Update Date : -
+
+# Arguments:
+# - dataset_name (str)       : 데이터셋 이름 ('cifar_10', 'fashion_mnist' or 'mnist')
+# - ae_encoder   (nn.Module) : Auto-Encoder 모델의 Encoder
+
+def run_hidden_representation_test(dataset_name, ae_encoder):
+    raise NotImplementedError
+
+
+if __name__ == '__main__':
+    dataset_names = ['cifar_10', 'fashion_mnist', 'mnist']
+
+    for dataset_name in dataset_names:
+        print(f'\n==== DATASET: {dataset_name} ====\n')
+
+        ae_encoder = load_ae_encoder(dataset_name)
+        ae_entire_model = load_ae_encoder(dataset_name)
+
+        run_reconstruction_test(dataset_name, ae_entire_model)
+        run_hidden_representation_test(dataset_name, ae_encoder)
