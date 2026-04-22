434facc20c07b903e4474b76fcc747127699dd98
wannabesuperteur <iwant_optimi@naver.com>
2026-04-22 23:29:34 +0900

    UPDATE: threshold cutoff experiment settings

--- 2025_10_06_OhLoRA_HP_Battle/hpo_training_model/hpo_training_model.py
+++ 2025_10_06_OhLoRA_HP_Battle/hpo_training_model/hpo_training_model.py
@@ -587,7 +587,7 @@ def run_threshold_cutoff_test():
         result_dict[f'input_features_{dataset_name}'] = []
 
     # threshold cutoff test
-    threshold_cutoffs = np.linspace(0.0, 0.3, 31)
+    threshold_cutoffs = np.linspace(0.0, 0.4, 41)
 
     for threshold_cutoff in threshold_cutoffs:
         start_at = time.time()
@@ -605,7 +605,7 @@ def run_threshold_cutoff_test():
 
         # save threshold cutoff test result
         result_df = pd.DataFrame(result_dict)
-        result_df.to_csv('hpo_model_test_result_per_corr_threshold_cutoff_new2.csv')
+        result_df.to_csv('hpo_model_test_result_per_corr_threshold_cutoff_new2_2.csv')
 
 
 if __name__ == '__main__':
