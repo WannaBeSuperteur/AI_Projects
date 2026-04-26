48ed377fbf41ca798c4aa900c374c66fcd3ccba6
wannabesuperteur <iwant_optimi@naver.com>
2026-04-26 09:33:11 +0900

    ADD: corr-coef threshold test log (5)

--- 2025_10_06_OhLoRA_HP_Battle/hpo_training_model/hpo_training_model.py
+++ 2025_10_06_OhLoRA_HP_Battle/hpo_training_model/hpo_training_model.py
@@ -612,7 +612,7 @@ def run_threshold_cutoff_test():
 
         # save threshold cutoff test result
         result_df = pd.DataFrame(result_dict)
-        result_df.to_csv('hpo_model_test_result_per_corr_threshold_cutoff_new2_4.csv')
+        result_df.to_csv('hpo_model_test_result_per_corr_threshold_cutoff_new2_5.csv')
 
 
 if __name__ == '__main__':
