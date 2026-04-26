6a5b7597659684fc09f0173f12631ca269953cd8
wannabesuperteur <iwant_optimi@naver.com>
2026-04-26 12:17:56 +0900

    UPDATE: dataset size 6600 -> 15600

--- 2025_10_06_OhLoRA_HP_Battle/hpo_training_data/create_hpo_model_train_data.py
+++ 2025_10_06_OhLoRA_HP_Battle/hpo_training_data/create_hpo_model_train_data.py
@@ -14,7 +14,7 @@ from hidden_representation.auto_encoder import AutoEncoderEncoder_1_28_28, AutoE
 
 
 PROJECT_DIR_PATH = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
-TRIALS_PER_DATASET = 6600
+TRIALS_PER_DATASET = 15600
 
 
 def initialize_data_dict():
