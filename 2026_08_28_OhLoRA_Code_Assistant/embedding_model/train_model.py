3740811184e5868c1bfca7469cdfc7ca9677843e
wannabesuperteur <iwant_optimi@naver.com>
2026-09-10 14:21:55 +0900

    UPDATE: learning rate

--- 2026_08_28_OhLoRA_Code_Assistant/embedding_model/train_model.py
+++ 2026_08_28_OhLoRA_Code_Assistant/embedding_model/train_model.py
@@ -84,7 +84,7 @@ class EmbeddingProbTrainer:
     def __init__(self, predictor: EmbeddingProbPredictor, data_loaders: dict):
         super().__init__()
         self.predictor = predictor
-        self.predictor.optimizer = torch.optim.AdamW(self.predictor.parameters(), lr=0.001)
+        self.predictor.optimizer = torch.optim.AdamW(self.predictor.parameters(), lr=3e-5)
         self.loss_func = nn.BCEWithLogitsLoss()
 
         self.data_loaders = data_loaders
