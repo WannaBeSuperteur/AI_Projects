8fc54f76e2029fca2b7b613ff521cf2fd1d87455
wannabesuperteur <iwant_optimi@naver.com>
2026-09-10 17:08:33 +0900

    ADD: similarity predict model train code

--- 2026_08_28_OhLoRA_Code_Assistant/embedding_model/train_model.py
+++ 2026_08_28_OhLoRA_Code_Assistant/embedding_model/train_model.py
@@ -6,6 +6,8 @@ import pandas as pd
 import sklearn
 
 import torch
+from sentence_transformers import SentenceTransformer, InputExample, losses
+from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator
 from torch.utils.data import Dataset, random_split, DataLoader
 import torch.nn as nn
 from transformers import AutoTokenizer, AutoModel
@@ -251,12 +253,113 @@ def train_probability_predictor(model_path: str, dataset_path: str, task_name: s
     trainer.run()
 
 
+def create_samples_and_dataloaders_for_tvt(dataset_df: pd.DataFrame):
+    dataset_size = len(dataset_df)
+    n_train_size = int(0.75 * dataset_size)
+    n_valid_size = int(0.125 * dataset_size)
+
+    def create_samples_and_dataloader(df: pd.DataFrame, shuffle: bool):
+        samples = [
+            InputExample(texts=[row['code_1'], row['code_2']], label=row['similarity'])
+            for _, row in df.iterrows()
+        ]
+        return samples, DataLoader(samples, shuffle=shuffle, batch_size=2)
+
+    train_df = dataset_df[:n_train_size]
+    valid_df = dataset_df[n_train_size:n_train_size + n_valid_size]
+    test_df = dataset_df[n_train_size + n_valid_size:]
+
+    _, train_dataloader = create_samples_and_dataloader(train_df, shuffle=True)
+    valid_samples, _ = create_samples_and_dataloader(valid_df, shuffle=False)
+    _, test_dataloader = create_samples_and_dataloader(test_df, shuffle=False)
+
+    return {
+        'train_loader': train_dataloader,
+        'test_loader': test_dataloader,
+        'valid_samples': valid_samples
+    }
+
+
+def test_similarity_predictor(model_dir_path: str, device: str, test_dataloader: DataLoader):
+    """test text embedding probability predictor."""
+
+    best_model = SentenceTransformer(model_dir_path, device=device)
+    best_model.eval()
+
+    true_labels = []
+    predicted_scores = []
+
+    with torch.no_grad():
+        for batch in test_dataloader:
+            texts1 = [ex.texts[0] for ex in batch]
+            texts2 = [ex.texts[1] for ex in batch]
+            labels = [ex.label for ex in batch]
+
+            embeddings1 = best_model.encode(texts1, convert_to_tensor=True, show_progress_bar=False)
+            embeddings2 = best_model.encode(texts2, convert_to_tensor=True, show_progress_bar=False)
+            cos_sim = best_model.similarity(embeddings1, embeddings2)
+            preds = torch.diagonal(cos_sim).cpu().numpy()
+
+            predicted_scores.extend(preds)
+            true_labels.extend(labels)
+
+            print(f'preds : {preds}')
+            print(f'labels : {true_labels}')
+
+    mse_result = sklearn.metrics.mean_squared_error(true_labels, predicted_scores)
+    return mse_result
+
+
+def train_similarity_predictor(model_path: str, dataset_path: str, task_name: str):
+    """train text embedding similarity predictor."""
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = SentenceTransformer(model_path, device=device)
+
+    dataset_df = pd.read_csv(dataset_path)
+    dataset_df = dataset_df.sample(frac=1)
+    samples_and_dataloaders = create_samples_and_dataloaders_for_tvt(dataset_df)
+
+    valid_samples = samples_and_dataloaders['valid_samples']
+    valid_evaluator = EmbeddingSimilarityEvaluator(
+        sentences1=[s.texts[0] for s in valid_samples],
+        sentences2=[s.texts[1] for s in valid_samples],
+        scores=[s.label for s in valid_samples],
+        name="valid-eval"
+    )
+
+    train_loss = losses.CosineSimilarityLoss(model=model)
+
+    model_dir_path = os.path.join(MODEL_SAVE_PATH, task_name)
+    os.makedirs(model_dir_path, exist_ok=True)
+
+    train_dataloader = samples_and_dataloaders['train_loader']
+    model.fit(
+        train_objectives=[(train_dataloader, train_loss)],
+        evaluator=valid_evaluator,
+        epochs=10,
+        evaluation_steps=10,
+        warmup_steps=5,
+        output_path=model_dir_path
+    )
+
+    test_dataloader = samples_and_dataloaders['test_loader']
+    test_similarity_predictor(model_dir_path, device, test_dataloader)
+
+
 if __name__ == '__main__':
     model_path = "codefuse-ai/F2LLM-v2-330M"
+
+#    dataset_path = os.path.join(PROJECT_DIR_PATH,
+#                                "code_reviewer",
+#                                "ai_dataset",
+#                                "dataset_01_func_docstring_single_responsibility.csv")
+#    task_name = "func_docstring_single_responsibility"
+#    train_probability_predictor(model_path, dataset_path, task_name)
+
     dataset_path = os.path.join(PROJECT_DIR_PATH,
                                 "code_reviewer",
                                 "ai_dataset",
-                                "dataset_01_func_docstring_single_responsibility.csv")
-    task_name = "func_docstring_single_responsibility"
-
-    train_probability_predictor(model_path, dataset_path, task_name)
+                                "dataset_01_return_matched_with_func_name.csv")
+    task_name = "return_matched_with_func_name"
+    train_similarity_predictor(model_path, dataset_path, task_name)
