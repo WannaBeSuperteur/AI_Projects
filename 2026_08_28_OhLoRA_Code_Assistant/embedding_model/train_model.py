afb1e61fd8d3cce010f73220bafd227948a6a210
wannabesuperteur <iwant_optimi@naver.com>
2026-09-25 01:49:17 +0900

    FIX: model exist handling

--- 2026_08_28_OhLoRA_Code_Assistant/embedding_model/train_model.py
+++ 2026_08_28_OhLoRA_Code_Assistant/embedding_model/train_model.py
@@ -356,7 +356,7 @@ def train_probability_predictor(model_path: str, dataset_path: str, task_name: s
     tokenizer.save_pretrained(model_dir_path)
     config.save_pretrained(model_dir_path)
 
-    if os.path.exists(model_dir_path):
+    if os.path.exists(model_dir_path) and os.listdir(model_dir_path):
         print(f'model already exists: {model_dir_path}')
         save_base_model(model_dir_path)
         return
@@ -475,7 +475,7 @@ def train_similarity_predictor(model_path: str, dataset_path: str, task_name: st
     """train text embedding similarity predictor."""
 
     model_dir_path = os.path.join(MODEL_SAVE_PATH, task_name)
-    if os.path.exists(model_dir_path):
+    if os.path.exists(model_dir_path) and os.listdir(model_dir_path):
         print(f'model already exists: {model_dir_path}')
         return
 
