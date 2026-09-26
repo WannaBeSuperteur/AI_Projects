b030b0f64e80304055ef237d712211aedbeb6649
wannabesuperteur <iwant_optimi@naver.com>
2026-09-26 20:35:53 +0900

    ADD: all test cases

--- 2026_08_28_OhLoRA_Code_Assistant/code_reviewer/test_cases/ai_test.py
+++ 2026_08_28_OhLoRA_Code_Assistant/code_reviewer/test_cases/ai_test.py
@@ -1,4 +1,7 @@
 
+import logging
+
+
 # 01_func_docstring_docstring_and_name
 def load_migration_plan():
     """Load the migration plan from the remote service.
@@ -106,3 +109,156 @@ new_schema_field_data = create_schema_field()
 calendar_slot_value = calculate_calendar_slot()
 unique_conversion_rate_data = deduplicate_conversion_rate()
 access_scope = classify_service_config()
+
+
+# 01_similar_variables
+local_shipment_profile_id, local_delivery_profile_identifier = 0, 1
+incoming_inventory_id, outgoing_inventory_id = 2, 3
+job_input_output, task_incoming_result = 4, 5
+local_statistic_id, remote_statistic_id = 6, 7
+validation_pipeline_summary_path, supplier_due_date = 8, 9
+
+
+# 01_unnecessary_prints
+logger = logging.getLogger("ai_test")
+
+output_path, database_restore_value, stage_number, total_stages, worker_count = 0, 1, 2, 3, 4
+invoice_export_records = [5]
+
+logger.info("Writing cache keys to %s", output_path)
+logger.debug(database_restore_value)
+logger.info("Tax calculation stage %s of %s finished", stage_number, total_stages)
+logger.info("Access review configured with %d workers", worker_count)
+logger.debug(list(invoice_export_records))
+
+
+# 02_numeric_values_maybe_const
+units_monthly, total, events, units_total, units = 0, 1, 2, 3, 4
+
+payload_yearly = units_monthly * 12
+balance = total - 160
+request_percent = events / units_total * 100
+
+
+def test_func(request: int):
+    return request * 75
+
+
+invoice = units % 21
+
+
+# 02_numeric_values_twice
+# TODO: update dataset
+
+
+# 04_func_args_bindable
+def func_args_bindable_0(publish_state):
+    return publish_state
+
+
+def func_args_bindable_1(client_id, client_secret, redirect_uri, scopes, dry_run, actor_id, idempotency_key):
+    return client_id + client_secret + redirect_uri + scopes + dry_run + actor_id + idempotency_key
+
+
+def func_args_bindable_2(locale, approval_required, purchase_order):
+    return locale + approval_required + purchase_order
+
+
+def func_args_bindable_3(red, green, blue):
+    return red + green + blue
+
+
+def func_args_bindable_4(country, state, county, city, neighborhood, strict_mode, logger, request_id):
+    return country + state + county + city + neighborhood + strict_mode + logger + request_id
+
+
+# 04_func_args_dynamic
+def func_args_dynamic_0(first_avatar_left, first_avatar_top, first_avatar_right, first_avatar_bottom,
+                        second_avatar_left, second_avatar_top, second_avatar_right, second_avatar_bottom):
+
+    line_1 = first_avatar_left + first_avatar_top + first_avatar_right + first_avatar_bottom
+    line_2 = second_avatar_left + second_avatar_top + second_avatar_right + second_avatar_bottom
+    return line_1 + line_2
+
+
+def func_args_dynamic_1(review_by_iman, review_by_jules, review_by_kai, review_by_lina, review_by_mina):
+    return review_by_iman + review_by_jules + review_by_kai + review_by_lina + review_by_mina
+
+
+def func_args_dynamic_2(card_foreground_red, card_foreground_green, card_foreground_blue,
+                        card_background_red, card_background_green, card_background_blue):
+
+    line_1 = card_foreground_red + card_foreground_green + card_foreground_blue
+    line_2 = card_background_red + card_background_green + card_background_blue
+    return line_1 + line_2
+
+
+def func_args_dynamic_3(modern_template, local_template, remote_template, public_template, private_template,
+                        front_template, back_template):
+
+    line_1 = modern_template + local_template + remote_template + public_template + private_template
+    line_2 = front_template + back_template
+    return line_1 + line_2
+
+
+def func_args_dynamic_4(grade_by_quinn, grade_by_ravi, grade_by_sora, grade_by_tariq, grade_by_uma, grade_by_victor):
+    return grade_by_quinn + grade_by_ravi + grade_by_sora + grade_by_tariq + grade_by_uma + grade_by_victor
+
+
+# 06_refactor_info_class_case_2_state_vars_if_else
+user_volume = 0
+if user_volume == 1:
+    print("A")
+elif user_volume == 2:
+    print("B")
+elif user_volume == 3:
+    print("C")
+elif user_volume == 4:
+    print("D")
+
+
+engine_termination = 0
+if engine_termination == 1:
+    print("A")
+elif engine_termination == 2:
+    print("B")
+elif engine_termination == 3:
+    print("C")
+elif engine_termination == 4:
+    print("D")
+
+
+router_checksum = 0
+if router_checksum == 1:
+    print("A")
+elif router_checksum == 2:
+    print("B")
+elif router_checksum == 3:
+    print("C")
+elif router_checksum == 4:
+    print("D")
+
+
+# 06_similar_function_names
+def audit_summary_persister():
+    print(0)
+
+
+def usage_summary_persister():
+    print(1)
+
+
+def authenticate_forecast():
+    print(2)
+
+
+def authorize_forecast():
+    print(3)
+
+
+def authorize_markdown_document():
+    print(4)
+
+
+def authorize_text_document():
+    print(5)
